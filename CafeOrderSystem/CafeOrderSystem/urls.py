88436e226b50c5bfd33e9df5d479fb11be08d689
Alexey Poltavskiy <79776177342@yandex.ru>
2025-03-04 18:23:07 +0300

    add main users urls

--- CafeOrderSystem/CafeOrderSystem/urls.py
+++ CafeOrderSystem/CafeOrderSystem/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Order-Meal/', include('orders.urls', namespace='orders')),
-    path('',include('frontend.urls', namespace='frontend'))
+    path('', include('frontend.urls', namespace='frontend')),
+    path('users/', include('users.urls', namespace='users')),
+
 ]
