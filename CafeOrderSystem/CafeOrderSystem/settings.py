32bcbae5dbf1fbbf0d7d4fea5fbdfdbe478cb538
Alexey <79776177342@yandex.ru>
2025-03-07 20:16:38 +0300

    minor structure changes

--- CafeOrderSystem/CafeOrderSystem/settings.py
+++ CafeOrderSystem/CafeOrderSystem/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'CafeOrderSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,6 +110,12 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
