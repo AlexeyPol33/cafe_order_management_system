85223da81012f003dc8cc1836e9cbc73f5d7a2b1
Alexey <79776177342@yandex.ru>
2025-03-07 03:12:14 +0300

    Revert "modified settings"
    This reverts commit a0f695efef65fb4892894084caa756c978db440e.

--- CafeOrderSystem/users/permissions.py
+++ CafeOrderSystem/users/permissions.py
@@ -1,10 +1,16 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 from .models import User
 
 
-class isAccountOwnerPermission(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_anonymous:
-            return False
-        
-        return request.user == obj.user
+class isAccountOwnerPermission(permissions.BasePermission):
+    pass
+
+
+class isOrderOwnerPermission(permissions.BasePermission):
+    pass
+
+class isStaffPermission(permissions.BasePermission):
+    pass
+
+class isManagerPermission(permissions.BasePermission):
+    pass
